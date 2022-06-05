db02a56ec2666849a515db4020fcc8c043795d2e
Gregory W <g.wasylow@gmail.com>
2022-06-06 01:22:59 +0200

    Main source code update.

--- TestSolutionCICD/TestSolutionCICD/Program.cs
+++ TestSolutionCICD/TestSolutionCICD/Program.cs
@@ -1,3 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World - we are now using CI/CD mechanism with GitHub Actions!");
 
