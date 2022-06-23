669de72d27d51bd48c953108a61c26526cdec7a1
Gregory W <g.wasylow@gmail.com>
2022-06-23 23:31:35 +0200

    Program.cs source update (recently tests have been failing).

--- TestSolutionCICD/TestSolutionCICD/Program.cs
+++ TestSolutionCICD/TestSolutionCICD/Program.cs
@@ -1,3 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World - we are now using CI/CD mechanism with GitHub Actions!" + zrobiłem błąd w kodzie!
+Console.WriteLine("Hello, World - we are now using CI/CD mechanism with GitHub Actions! Przy okazji naprawilem blad wskazny w mechanizmie CI");
 
